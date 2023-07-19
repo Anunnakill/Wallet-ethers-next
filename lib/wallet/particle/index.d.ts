57f0a21b0f86a8b6a425acbb54734b4c9a66a4b6
Anunnakill <jicheng.eleven@gmail.com>
2023-07-19 21:36:51 +0800

    update

---
+++ lib/wallet/particle/index.d.ts
@@ -0,0 +1,17 @@
+import { Chain } from "@particle-network/common";
+import { JsonRpcSigner, Eip1193Provider } from "ethers";
+import { IParticleConfig } from "./interface";
+declare class Particle {
+    address: string;
+    signer: JsonRpcSigner;
+    web3Provider: Eip1193Provider;
+    private particle;
+    private particleConfig;
+    constructor(particleConfig: IParticleConfig);
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    onAccountsChanged(callBack: Function): void;
+    onChainChanged(callBack: Function): void;
+    switchEthereumChain(params: Chain): Promise<void>;
+}
+export { Particle };
